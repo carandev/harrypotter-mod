d781ee93474208c0d993d3713e4a301cc50cf9d9
Carlos Gomez <carlosandres0741@gmail.com>
2024-10-20 22:28:01 -0500

    feat: create project

---
+++ src/client/kotlin/me/carandev/harrypotter/client/HarryPotterClient.kt
@@ -0,0 +1,9 @@
+package me.carandev.harrypotter.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class HarryPotterClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
---
+++ src/main/kotlin/me/carandev/harrypotter/HarryPotter.kt
@@ -0,0 +1,9 @@
+package me.carandev.harrypotter
+
+import net.fabricmc.api.ModInitializer
+
+class HarryPotter : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
